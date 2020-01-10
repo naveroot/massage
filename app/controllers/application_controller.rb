5a2499b4d46a7a1caaa801811a785fb9baa33b3e
naveroot <naveroot@gmailcom>
2020-01-10 16:39:45 +0300

    add i18l

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :switch_locale
+
+  def switch_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_option
+    { locale: I18n.locale }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'welcome/index'
-  root to: 'welcome#index'
+  scope "(:locale)", locale: /en|ru/ do
+    get "welcome/index"
+    root to: 'welcome#index'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
