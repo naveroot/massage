e8288c21de72d3949c72937d7aaa46ea9cde5617
naveroot <naveroot@gmailcom>
2020-01-16 11:08:12 +0300

    save messages from bot to db

--- app/services/telegram.rb
+++ app/services/telegram.rb
@@ -2,6 +2,6 @@ require 'open-uri'
 
 class Telegram
   def self.set_webhook(token, prefix)
-    open("https://api.telegram.org/#{token}/setWebhook?url=https://34267474.ngrok.io/webhooks/telegram_#{prefix}")
+    open("https://api.telegram.org/#{token}/setWebhook?url=https://agile-ocean-36103.herokuapp.com/webhooks/telegram_#{prefix}")
   end
 end
